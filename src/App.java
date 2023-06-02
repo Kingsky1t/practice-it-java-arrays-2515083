29198c0ca98f7adde52cb0b4b5e3e5a5ffe5c02f
Shaunak Chandra <79399986+Kingsky1t@users.noreply.github.com>
2023-06-02 12:50:09 +0530

    implements rotate right

--- src/App.java
+++ src/App.java
@@ -1,14 +1,33 @@
 import java.util.Arrays;
 
 public class App {
-  
+
+  // in-place
+  // public static Object[] rotateRight(Object[] arr) {
+  // Object temp = arr[arr.length-1];
+  // for(int i=arr.length-1;i>0;i--) {
+  // arr[i]=arr[i-1];
+  // }
+  // arr[0]=temp;
+  // return arr;
+  // }
+
+  // new array
   public static Object[] rotateRight(Object[] arr) {
-	  return arr;
+    if (arr==null) {
+      return null;
+    }
+    
+    Object[] res = new Object[arr.length];
+    for (int i = 0; i < arr.length; i++) {
+      res[(i + 1) % res.length] = arr[i];
+    }
+    return res;
   }
-  
+
   public static void main(String args[]) {
-    Object[] arr = new Integer[] {1, 2, 3, 4, 5};
-      
+    Object[] arr = new Integer[] { 1, 2, 3, 4, 5 };
+
     arr = rotateRight(arr);
     System.out.println(Arrays.toString(arr));
     arr = rotateRight(arr);
@@ -19,9 +38,9 @@ public class App {
     System.out.println(Arrays.toString(arr));
     arr = rotateRight(arr);
     System.out.println(Arrays.toString(arr));
-    
-    Object[] arr2 = new String[] {"Hello", "there", "my friend"};
-    
+
+    Object[] arr2 = new String[] { "Hello", "there", "my friend" };
+
     arr2 = rotateRight(arr2);
     System.out.println(Arrays.toString(arr2));
     arr2 = rotateRight(arr2);
